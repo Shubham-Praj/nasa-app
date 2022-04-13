a4074b4501a51e8b099bd7cac445e60cb45b4814
shubhampraj <shubhampraj11@gmail.com>
2022-04-13 18:02:03 +0530

    UI completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Background from "./Components/Background";
 import ContentDiv from "./Components/ContentDiv";
 import Header from "./Components/Header";
 
 function App() {
+  const [apodData, setapodData] = useState([]);
+
+  //Function to get data from the nasa api
+  async function getApodData(date) {
+    const res = await fetch(
+      `${process.env.REACT_APP_BASE_URL}date=${date}&&api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    const data = await res.json();
+    setapodData(data);
+  }
+
+  useEffect(() => {
+    getApodData("2017-01-02");
+  }, []);
+
   return (
-    <div style={{"display" : "flex", "justifyContent" : "center"}}>
-     <Background/>
-     <Header/>
-     <ContentDiv/>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Background />
+      <Header />
+      <ContentDiv data={apodData} />
     </div>
   );
 }
--- src/Components/ContentDiv.js
+++ src/Components/ContentDiv.js
@@ -1,36 +1,27 @@
 import React from "react";
 
-function ContentDiv() {
+function ContentDiv({data}) {
+
+  console.log("Content div data :- " , data);
+
   return (
     <div className="content-main-div">
       <div className="content-data-div">
         <div className="picture-title">
-          <h2>A Picturesque Equinox Sunset</h2>
+          <h2>{data.title}</h2>
         </div>
         <div className="explanation">
           <p>
-            What's that at the end of the road? The Sun. Many towns have roads
-            that run east - west, and on two days each year, the Sun rises and
-            sets right down the middle. Today is one of those days: an equinox.
-            Not only is today a day of equal night (\"aequus\"-\"nox\") and day
-            time, but also a day when the sun rises precisely to the east and
-            sets due west. Featured here is a picturesque road in northwest
-            Illinois, USA that runs approximately east -west. The image was
-            taken during the March Equinox of 2015, and shows the Sun down the
-            road at sunset. In many cultures, this March equinox is taken to be
-            the first day of a season, typically spring in Earth's northern
-            hemisphere, and autumn in the south. Does your favorite street run
-            east - west? Tonight, at sunset, you can find out with a quick
-            glance.
+            {data.explanation}
           </p>
         </div>
       </div>
       <div className="content-image-div">
-        <div className="image">
-          <img style={{width : "100%" , height : "100%"}} src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt=""/>
+        <div className="image-div">
+          <img className="image" src={ data.url || data.hdurl} alt=""/>
         </div>
-        <div className="author">
-            <h3>adhsalksd</h3>
+        <div className="author-div">
+            <h3>{data.copyright}</h3>
         </div>
       </div>
     </div>
